bb48af8325d59b066ffb2fd88dab2ab6c3e0c2ba
Suzanne Dupéron <ligo@suzanne.soy>
2020-01-06 19:30:34 +0100

    Rename partition → UnionFind for consistency across the various names that opam and dune assign to this library

---
+++ unionFind.ml
@@ -0,0 +1,6 @@
+(* This module is the public interface and corresponds to public_name in dune *)
+module Partition = Partition
+module Partition0 = Partition0
+module Partition1 = Partition1
+module Partition2 = Partition2
+module Partition3 = Partition3
